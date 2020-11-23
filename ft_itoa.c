27035bfee71236f2619fe2cf8a6e74a503f6dd18
ximure <ximure@mail.ru>
2020-11-23 20:28:41 +0300

    hecking heck

--- ft_itoa.c
+++ ft_itoa.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: tmarei <tmarei@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/23 20:17:22 by tmarei            #+#    #+#             */
-/*   Updated: 2020/11/23 20:17:25 by tmarei           ###   ########.fr       */
+/*   Created: 2020/11/19 16:52:36 by tmarei            #+#    #+#             */
+/*   Updated: 2020/11/21 16:39:26 by tmarei           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,9 @@ char	*ft_strnew(size_t size)
 {
 	char *str;
 
-	str = (char *)malloc((size + 1) * sizeof(char));
-	if (str)
+	if (!(str = (char *)malloc((size + 1) * sizeof(char))))
+		return (NULL);
+	if (1)
 	{
 		ft_bzero(str, size + 1);
 		return (str);
@@ -25,31 +26,31 @@ char	*ft_strnew(size_t size)
 	return (NULL);
 }
 
-char	*ft_itoa(int nbr)
+char	*ft_itoa(int n)
 {
 	int		length;
 	int		sign;
-	char	*str;
+	char	*s;
 
-	sign = nbr;
+	sign = n;
 	length = 1;
 	while (sign /= 10)
 		length++;
-	sign = nbr < 0 ? 1 : 0;
-	length = nbr < 0 ? length += 1 : length;
-	if (nbr == -2147483648)
-		return (str = ft_strdup("-2147483648"));
-	str = ft_strnew(length);
-	if (!str)
+	sign = n < 0 ? 1 : 0;
+	length = n < 0 ? length += 1 : length;
+	if (n == -2147483648)
+		return (s = ft_strdup("-2147483648"));
+	s = ft_strnew(length);
+	if (!s)
 		return (NULL);
 	if (sign)
-		str[0] = '-';
-	nbr = nbr < 0 ? nbr *= -1 : nbr;
+		s[0] = '-';
+	n = n < 0 ? n *= -1 : n;
 	while (--length >= sign)
 	{
-		str[length] = (nbr >= 10) ? (nbr % 10) + 48 : nbr + 48;
-		nbr /= 10;
+		s[length] = (n >= 10) ? (n % 10) + 48 : n + 48;
+		n /= 10;
 	}
-	str[ft_strlen(str)] = '\0';
-	return (str);
+	s[ft_strlen(s)] = '\0';
+	return (s);
 }
